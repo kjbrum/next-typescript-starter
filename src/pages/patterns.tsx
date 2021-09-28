a96e4183c300837a828a8804295c2a2d18c01582
Kyle Brumm <kjbrumm@gmail.com>
2021-09-28 14:26:50 -0500

    Fix image height in patterns

--- src/pages/patterns.tsx
+++ src/pages/patterns.tsx
@@ -185,7 +185,7 @@ const Patterns: NextPage = () => {
                         Image will stretch both width and height to the
                         dimensions of the parent element.
                     </Text>
-                    <Box className="relative w-full h-[105px]">
+                    <Box className="relative w-full h-[150px]">
                         <Image
                             src="https://source.unsplash.com/OmEV7k3OniM/1000x600"
                             layout="fill"
